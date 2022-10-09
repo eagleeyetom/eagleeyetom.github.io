903a891face72a9cb9e21a4dd84059fd079ba91e
eagleeyetom <tomek.kondrat@gmail.com>
2022-10-09 18:56:07 +0200

    2022 here we go

---
+++ js/accessibility.js
@@ -0,0 +1,15 @@
+window.onload
+$("#fontPlusBtn").click(function () {
+    $(".textDiv > *").css("font-size", function (i, value) {
+        return parseInt(value) + 5;
+    });
+});
+$("#fontMinusBtn").click(function () {
+    $(".textDiv > *").css("font-size", function (i, value) {
+        return parseInt(value) - 5;
+    });
+});
+
+function switchColor() {
+    document.getElementById("resultDiv").classList.toggle("toggle")
+}
