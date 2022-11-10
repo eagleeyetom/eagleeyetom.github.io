7e4e1a821d8b53cc9bd4f1b491f09c2ee07983d8
eagleeyetom <tomek.kondrat@gmail.com>
2022-11-10 19:57:10 +0100

    up

--- js/preload.js
+++ js/preload.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){   
-    window.setTimeout('fadeout();', 3000);
-    
-});
-
-function fadeout(){
-    $('.container').delay(2000).fadeOut('slow', function() {
-       $('.preload').removeClass('preload');
-    });
-}
+//$(document).ready(function(){   
+//    window.setTimeout('fadeout();', 3000);
+//    
+//});
+//
+//function fadeout(){
+//    $('.container').delay(2000).fadeOut('slow', function() {
+//       $('.preload').removeClass('preload');
+//    });
+//}
