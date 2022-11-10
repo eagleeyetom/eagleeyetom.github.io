34af52043bbc87463a9ecbe01a29beb12e3aa5cd
eagleeyetom <tomek.kondrat@gmail.com>
2022-11-10 16:57:57 +0100

    footer i preload

---
+++ js/preload.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){   
+    window.setTimeout('fadeout();', 3000);
+    
+});
+
+function fadeout(){
+    $('.container').delay(2000).fadeOut('slow', function() {
+       $('.preload').removeClass('preload');
+    });
+}
