380efbd45da79f00b536f79911c1b65d2cfc2356
eagleeyetom <tomek.kondrat@gmail.com>
2022-11-11 22:31:43 +0100

    YT 2022

--- js/accessibility.js
+++ js/accessibility.js
@@ -1,15 +1,16 @@
-window.onload
+window.onload;
 $("#fontPlusBtn").click(function () {
-    $(".textDiv > *").css("font-size", function (i, value) {
-        return parseInt(value) + 5;
-    });
+  $(".textDiv > *").css("font-size", function (i, value) {
+    return parseInt(value) + 5;
+  });
 });
 $("#fontMinusBtn").click(function () {
-    $(".textDiv > *").css("font-size", function (i, value) {
-        return parseInt(value) - 5;
-    });
+  $(".textDiv > *").css("font-size", function (i, value) {
+    return parseInt(value) - 5;
+  });
 });
 
 function switchColor() {
-    document.getElementById("resultDiv").classList.toggle("toggle")
+  document.getElementById("resultDiv").classList.toggle("toggle");
 }
+//$.cookie("switchColor", "true", { expires: 7 });
