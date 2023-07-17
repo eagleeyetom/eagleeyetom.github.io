0e9e87ebd775c3b64112a96f0479782730060047
eagleeyetom <tomek.kondrat@gmail.com>
2023-07-17 23:20:12 +0200

    up

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,37 @@
+function acceptCookies() {
+  setCookie("cookiesAccepted", "true", 365);
+  var cookieInfo = document.querySelector(".cookie-info");
+  cookieInfo.style.display = "none";
+}
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var cookieArr = document.cookie.split("; ");
+  for (var i = 0; i < cookieArr.length; i++) {
+    var cookiePair = cookieArr[i].split("=");
+    if (name === cookiePair[0]) {
+      return decodeURIComponent(cookiePair[1]);
+    }
+  }
+  return null;
+}
+
 window.onload = function () {
+  var cookiesAccepted = getCookie("cookiesAccepted");
+  if (cookiesAccepted) {
+    var cookieInfo = document.querySelector(".cookie-info");
+    cookieInfo.style.display = "none";
+  }
+
   var fontSizeChoice = getCookie("fontSizeChoice");
   if (
     fontSizeChoice &&
@@ -34,26 +67,4 @@ function toggleContrast() {
     ? "high-contrast"
     : "default";
   setCookie("contrastChoice", contrastChoice, 365);
-}
-
-function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = "; expires=" + date.toUTCString();
-  }
-  // Add the SameSite attribute to control cross-site behavior
-  document.cookie = name + "=" + value + expires + "; path=/; SameSite=Lax";
-}
-
-function getCookie(name) {
-  var cookieArr = document.cookie.split("; ");
-  for (var i = 0; i < cookieArr.length; i++) {
-    var cookiePair = cookieArr[i].split("=");
-    if (name === cookiePair[0]) {
-      return decodeURIComponent(cookiePair[1]);
-    }
-  }
-  return null;
 }
