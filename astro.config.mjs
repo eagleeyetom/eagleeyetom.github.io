dd279a08333e70c26faf0fc3075da1b9e6c1a23c
eagleeyetom <tomek.kondrat@gmail.com>
2026-07-10 22:15:28 +0200

    automatyczna generacja sitemap za pomoca @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://masska.org',
   output: 'static',
   build: {
     format: 'file'
-  }
+  },
+  integrations: [sitemap()]
 });
